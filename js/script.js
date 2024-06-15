83c92544af8ed0af02a3a936fb824622822a27cf
mnaamin <mna.amin.21@gmail.com>
2024-06-15 12:08:15 +0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,15 @@
+// toggle class active
+const navbarNav = document.querySelector(".navbar-nav");
+// ketika humberger menu di klik
+document.querySelector("#hamburger-menu").onclick = () => {
+  navbarNav.classList.toggle("active");
+};
+
+//klik di luar sidebag untuk menghilangkan nav
+const hamburger = document.querySelector("#hamburger-menu");
+
+document.addEventListener("click", function (e) {
+  if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove("active");
+  }
+});
